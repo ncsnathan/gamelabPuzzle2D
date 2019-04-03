f4d9606a9fba6d396e49dfa8dda1d935c7b4683b
Nathan Santana <nathan_ncs@outlook.com>
2019-04-03 09:30:00 -0300

    Construcao do Layout e Respawn da primeira peca

---
+++ Assets/Scripts/respawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    float cont;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cont = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cont += Time.deltaTime;
+        if(cont>=2)
+        {
+            Respawn();
+            cont = 0f;
+        }
+    }
+
+    private void Respawn()
+    {
+        GameObject gameBlock = Resources.Load("Blue") as GameObject;
+        Instantiate(gameBlock);
+    }
+}
